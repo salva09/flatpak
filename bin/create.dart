fb8661c1957d294ed2bdde6f342b70636f553353
Salva HG <salva@pop-os.localdomain>
2022-02-03 18:51:44 -0600

    Initial commit

---
+++ bin/create.dart
@@ -0,0 +1,5 @@
+import 'package:flatpak/flatpak.dart';
+
+void main(List<String> arguments) {
+  Flatpak().createFlatpak(arguments);
+}
---
+++ lib/flatpak.dart
@@ -0,0 +1,12 @@
+
+import 'dart:async';
+
+class Flatpak {
+  Future<void> createFlatpak(List<String> cliArguments) async {
+    print(testing());
+  }
+
+  static String testing() {
+    return "Hello from flatpak";
+  }
+}
